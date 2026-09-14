cd12934b467e44761dbed2d0c5edfa8485335a20
jamilarabi2000-del <jamilarabi2000@gmail.com>
2026-09-14 03:06:21 +0300

    remove Firebase functions from Supabase migration

--- functions/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { placeOrder } from './placeOrder.js';
-export { checkPhoneAvailability, recordAdminStepUp } from './checkPhone.js';
-export { submitSellerApplication, onSellerApplicationWritten } from './sellerApplication.js';
-export { bootstrapAdmin } from './adminBootstrap.js';
