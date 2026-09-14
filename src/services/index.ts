49682486d573ac570f517cba83057ba70896f607
jamilarabi2000-del <jamilarabi2000@gmail.com>
2026-09-14 03:41:01 +0300

    fix: export restored Supabase admin and commerce services

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from './supabaseCatalogService';
 export * from './supabaseUserDataService';
 export * from './supabaseOrderService';
 export * from './supabaseCmsService';
+export * from './supabaseAdminService';
+export * from './supabaseCommerceService';
