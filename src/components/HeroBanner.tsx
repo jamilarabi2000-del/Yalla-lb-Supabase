63ddec8847bc7e71262f862a727273043260c261
jamilarabi2000-del <jamilarabi2000@gmail.com>
2026-09-22 03:15:43 +0300

    Register storefront element for universal CMS design control

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -462,8 +462,7 @@ export const HeroBanner: React.FC = () => {
     containerHeightClass += 'md:w-full md:h-[580px] lg:h-[620px] ';
   }
 
-  return (
-    <div className="w-full max-w-full md:max-w-screen-2xl md:mx-auto md:px-4 sm:md:px-6 lg:px-8 md:pt-4 md:pb-2">
+  return (\n    <div data-cms-element="hero" className="w-full max-w-full md:max-w-screen-2xl md:mx-auto md:px-4 sm:md:px-6 lg:px-8 md:pt-4 md:pb-2">
       <div 
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
