e5ad618c613b667c2f3e88e53520269ef666ee10
jamilarabi2000-del <jamilarabi2000@gmail.com>
2026-09-22 03:15:48 +0300

    Register storefront element for universal CMS design control

--- src/components/HomeView.tsx
+++ src/components/HomeView.tsx
@@ -691,8 +691,7 @@ export const HomeView: React.FC = () => {
     }
   };
 
-  return (
-    <div className="pb-12 bg-[#F7F6F1] pt-4 sm:pt-6">
+  return (\n    <div data-cms-element="home" className="pb-12 bg-[#F7F6F1] pt-4 sm:pt-6">
       
       {/* Top Custom Divs / Banners */}
       <CustomBlocksRenderer page="home" position="top" />
